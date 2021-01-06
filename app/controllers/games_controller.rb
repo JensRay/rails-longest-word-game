cdf671c4727ae3b09c3081aafeeafee4bd64cbd8
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2021-01-06 18:50:14 +0100

    done without optional part

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,28 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
+  def new
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
+  end
+
+  def score
+    @word = params[:word].upcase
+    @letters = params[:letters]
+    @include = includes?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  def includes?(word, letters)
+    word.split("").all? { |l| letters.include?(l) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json["found"]
+  end
 end
